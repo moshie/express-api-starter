7d1f3e849be744d9f570bb9f88915ab9486c386c
David Hewitt <david.hewitt@indigotree.co.uk>
2018-07-13 15:21:53 +0100

    Added comment placeholders in place of endpoints

--- app/api/permissions/index.js
+++ app/api/permissions/index.js
@@ -15,9 +15,19 @@ const router = express.Router();
 //
 const authenticate = require('./middleware/authenticate');
 
-/* POST Login */
+/*
 
+# PERMISSIONS
+POST    /permissions/                   -- Create a new permission
+GET     /permissions/                   -- Get all permissions
+GET     /permissions/:permissions_id/   -- Get permission by id
+PUT     /permissions/:permissions_id/   -- Update a permission
+DELETE  /permissions/:permissions_id/   -- Delete a permission
 
+PUT     /permissions/:permission_id/roles/:role_id/     -- Assign a permission to a role
+DELETE  /permissions/:permission_id/roles/:role_id/     -- Revoke a permission from a role
+
+*/
 
 
 module.exports = router;
--- app/api/users/index.js
+++ app/api/users/index.js
@@ -5,11 +5,29 @@ const storeValidator = require('./validators/store-validator');
 
 const storeController = require('./controllers/store-controller');
 
-// Needs Auth
+
+/*
+NEEDS AUTH!
+
+# USERS
+POST    /users/             -- Create a new User
+GET     /users/             -- Get all users
+GET     /users/:user_id/    -- Get user by id
+PUT     /users/:user_id/    -- Update a user
+DELETE  /users/:user_id/    -- Delete a user
+
+GET     /users/:user_id/roles/              -- Get a users roles
+PUT     /users/:user_id/roles/:role_id/     -- Assign a user a role
+DELETE  /users/:user_id/roles/:role_id/     -- Revoke a user a role
+
+GET     /users/:user_id/permissions/        -- Get a users current permissions
+
+GET     /users/:user_id/confirmed/          -- Checks if user is confirmed or not
+
+*/
+
 
 /* POST store */
 router.post('/', storeValidator, storeController);
-
-// is_confirmed
 
 module.exports = router;
