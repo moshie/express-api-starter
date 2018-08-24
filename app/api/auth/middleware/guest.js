632293b76e72b4acb86887cbf2d8274d4635648b
David Hewitt <david.hewitt@indigotree.co.uk>
2018-08-24 10:15:17 +0100

    updated guest middleware to make it more testable

--- app/api/auth/middleware/guest.js
+++ app/api/auth/middleware/guest.js
@@ -2,14 +2,12 @@
 
 function authenticate(req, res, next) {
 
-    function badRequest(message = 'Bad Request') {
-        res.status(403).json({
-            data: { message }
-        });
-    }
-
     if (req.get('authorization')) {
-        return badRequest('You are not authorised to view this page');
+        return res.status(403).json({
+            meta: { 
+                message: 'You are not authorised to view this page'
+            }
+        });
     }
 
     next();
